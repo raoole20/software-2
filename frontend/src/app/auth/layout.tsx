30f640adf34a05138a9135964809b5d5f89df134
raul espina <respina@bigwise.com>
2025-10-02 09:07:10 -0400

    feat: remove authentication navigation and tabs, redirect to login page

--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
-import AuthNav from "./components/nav";
-import TabAuth from "./components/tab-auth/TabAuth";
+
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <section className="min-h-screen flex flex-col gap-6">
-            <AuthNav />
-            
-            <div className="flex flex-1 flex-col justify-center items-center px-4">
-                <TabAuth>
-                    {children}
-                </TabAuth>
-            </div>
+
+           
         </section>
     );
 }
